fa7ffe5dbfebb73e41ccb2c8f1c7fad0664313cc
nialamejor73 <nialamejor73@hotmail.com>
2017-07-19 11:58:24 -0400

    resta, multip y div

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -4,5 +4,11 @@ public class Main {
   
   public static void main(String[] args) {
     System.out.println("Hola mi amorr ajjaja");
+    int s = suma(20,30);
+    System.out.println(s);
+  }
+  
+  public static int suma(int n1, int n2) {
+    return n1 + n2;
   }
 }
